270ec1f5a2ad8f6c280ec67e15b2b0151ac1149b
Simone Siciliano <YYI5097@ic.intra.infocamere.it>
2022-03-04 02:51:52 +0100

    Inizio Integrazione modulo frontend in angular

--- baseWeb/src/main/java/it/home/base/base/web/baseWeb/controllers/indexController.java
+++ baseWeb/src/main/java/it/home/base/base/web/baseWeb/controllers/indexController.java
@@ -2,11 +2,11 @@ package it.home.base.base.web.baseWeb.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+/*
 @Controller
 public class indexController {
     @RequestMapping(value = "/")
     public String index() {
         return "index";
     }
-}
+}*/
